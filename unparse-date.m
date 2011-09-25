ff676377d059db6a88d1d4ec163a4cf24d380332
Peter Hosey <hg@boredzo.org>
2011-09-25 15:07:38 -0700

    Beefed up the calendar-date unparser test with a couple of command-line arguments.

--- unparse-date.m
+++ unparse-date.m
@@ -6,10 +6,26 @@ int main(int argc, const char **argv) {
 	ISO8601DateFormatter *formatter = [[[ISO8601DateFormatter alloc] init] autorelease];
 	formatter.format = ISO8601DateFormatCalendar;
 
+	BOOL forceUTC = NO;
+
+	while (argv[1]) {
+		if (strcmp(argv[1], "--include-time") == 0)
+			formatter.includeTime = YES;
+		else if (strcmp(argv[1], "--force-utc") == 0)
+			forceUTC = YES;
+		else
+			break;
+		--argc;
+		++argv;
+	}
+
 	while(--argc) {
 		NSString *arg = [NSString stringWithUTF8String:*++argv];
 		NSTimeZone *timeZone = nil;
-		printf("%s\n", [[NSString stringWithFormat:@"%@:\t%@", arg, [formatter stringFromDate:[formatter dateFromString:arg timeZone:&timeZone] timeZone:timeZone]] UTF8String]);
+		NSDate *date = [formatter dateFromString:arg timeZone:&timeZone];
+		if (forceUTC)
+			timeZone = [NSTimeZone timeZoneWithAbbreviation:@"UTC"];
+		printf("%s\n", [[NSString stringWithFormat:@"%@:\t%@", arg, [formatter stringFromDate:date timeZone:timeZone]] UTF8String]);
 	}
 
 	[pool release];
