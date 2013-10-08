f17713f8361130207e16df77c3cd25110b31ea24
Peter Hosey <git@boredzo.org>
2013-10-08 03:30:29 -0700

    Greatly expanded the documentation. Fixes #27.

--- ISO8601DateFormatter.h
+++ ISO8601DateFormatter.h
@@ -6,32 +6,37 @@
 
 #import <Foundation/Foundation.h>
 
-/*This class converts dates to and from ISO 8601 strings. A good introduction to ISO 8601: <http://www.cl.cam.ac.uk/~mgk25/iso-time.html>
+///Which of ISO 8601's three date formats the formatter should produce.
+typedef NS_ENUM(NSUInteger, ISO8601DateFormat) {
+	///YYYY-MM-DD.
+	ISO8601DateFormatCalendar,
+	///YYYY-DDD, where DDD ranges from 1 to 366; for example, 2009-32 is 2009-02-01.
+	ISO8601DateFormatOrdinal,
+	///YYYY-Www-D, where ww ranges from 1 to 53 (the 'W' is literal) and D ranges from 1 to 7; for example, 2009-W05-07.
+	ISO8601DateFormatWeek,
+};
+
+///The default separator for time values. Currently, this is ':'.
+extern const unichar ISO8601DefaultTimeSeparatorCharacter;
+
+/*!
+ *This class converts dates to and from ISO 8601 strings. A good introduction to ISO 8601 is [“A summary of the international standard date and time notation” by Markus Kuhn](http://www.cl.cam.ac.uk/~mgk25/iso-time.html).
  *
- *Parsing can be done strictly, or not. When you parse loosely, leading whitespace is ignored, as is anything after the date.
- *The loose parser will return an NSDate for this string: @" \t\r\n\f\t  2006-03-02!!!"
- *Leading non-whitespace will not be ignored; the string will be rejected, and nil returned. See the README that came with this addition.
+ *Parsing can be done strictly, or not.
  *
- *The strict parser will only accept a string if the date is the entire string. The above string would be rejected immediately, solely on these grounds.
- *Also, the loose parser provides some extensions that the strict parser doesn't.
+ *When you parse strictly, the parser will only accept a string if the date is the entire string.
+ *
+ *When you parse loosely, leading whitespace is ignored, as is anything after the date.
+ *The loose parser will return an NSDate for this string: `@" \t\r\n\f\t  2006-03-02!!!"`
+ *Some of the methods take a pointer to an NSRange, with which they will tell you what part of the string was the date.
+ *
+ *Leading non-whitespace is never ignored; the string will be rejected, and `nil` returned. See the README that came with this addition.
+ *
+ *The loose parser provides some extensions that the strict parser doesn't.
  *For example, the standard says for "-DDD" (an ordinal date in the implied year) that the logical representation (meaning, hierarchically) would be "--DDD", but because that extra hyphen is "superfluous", it was omitted.
  *The loose parser will accept the extra hyphen; the strict parser will not.
  *A full list of these extensions is in the README file.
  */
-
-/*The format to either expect or produce.
- *Calendar format is YYYY-MM-DD.
- *Ordinal format is YYYY-DDD, where DDD ranges from 1 to 366; for example, 2009-32 is 2009-02-01.
- *Week format is YYYY-Www-D, where ww ranges from 1 to 53 (the 'W' is literal) and D ranges from 1 to 7; for example, 2009-W05-07.
- */
-typedef NS_ENUM(NSUInteger, ISO8601DateFormat) {
-	ISO8601DateFormatCalendar,
-	ISO8601DateFormatOrdinal,
-	ISO8601DateFormatWeek,
-};
-
-//The default separator for time values. Currently, this is ':'.
-extern const unichar ISO8601DefaultTimeSeparatorCharacter;
 
 @interface ISO8601DateFormatter: NSFormatter
 
@@ -41,24 +46,105 @@ extern const unichar ISO8601DefaultTimeSeparatorCharacter;
 
 //As a formatter, this object converts strings to dates.
 
+///If set to `YES`, disables various leniencies in how the formatter parses strings. Does not affect unparsing.
 @property BOOL parsesStrictly;
 
+/*!
+ *	@brief	Parse a string into individual date components.
+ *
+ *	@param	string	The string to parse. Must represent a date in one of the ISO 8601 formats.
+ *	@returns	An NSDateComponents object containing most of the information parsed from the string, aside from the fraction of second and time zone (which are lost).
+ */
 - (NSDateComponents *) dateComponentsFromString:(NSString *)string;
+/*!
+ *	@brief	Parse a string into individual date components.
+ *
+ *	@param	string	The string to parse. Must represent a date in one of the ISO 8601 formats.
+ *	@param	outTimeZone	If non-`NULL`, an NSTimeZone object or `nil` will be stored here, depending on whether the string specified a time zone.
+ *	@returns	An NSDateComponents object containing most of the information parsed from the string, aside from the fraction of second (which is lost) and time zone.
+ */
 - (NSDateComponents *) dateComponentsFromString:(NSString *)string timeZone:(out NSTimeZone **)outTimeZone;
+/*!
+ *	@brief	Parse a string into individual date components.
+ *
+ *	@param	string	The string to parse. Must represent a date in one of the ISO 8601 formats.
+ *	@param	outTimeZone	If non-`NULL`, an NSTimeZone object or `nil` will be stored here, depending on whether the string specified a time zone.
+ *	@param	outRange	If non-`NULL`, an NSRange structure will be stored here, identifying the substring of `string` that specified the date.
+ *	@param	outFractionOfSecond	If non-`NULL`, an NSTimeInterval value will be stored here, containing the fraction of a second, if the string specified one. If it didn't, this will be set to zero.
+ *	@returns	An NSDateComponents object containing most of the information parsed from the string, aside from the fraction of second and time zone.
+ */
 - (NSDateComponents *) dateComponentsFromString:(NSString *)string timeZone:(out NSTimeZone **)outTimeZone range:(out NSRange *)outRange fractionOfSecond:(NSTimeInterval *)outFractionOfSecond;
 
+/*!
+ *	@brief	Parse a string.
+ *
+ *	@param	string	The string to parse. Must represent a date in one of the ISO 8601 formats.
+ *	@returns	An NSDate object containing most of the information parsed from the string, aside from the time zone (which is lost).
+ */
 - (NSDate *) dateFromString:(NSString *)string;
+/*!
+ *	@brief	Parse a string.
+ *
+ *	@param	string	The string to parse. Must represent a date in one of the ISO 8601 formats.
+ *	@param	outTimeZone	If non-`NULL`, an NSTimeZone object or `nil` will be stored here, depending on whether the string specified a time zone.
+ *	@returns	An NSDate object containing most of the information parsed from the string, aside from the time zone.
+ */
 - (NSDate *) dateFromString:(NSString *)string timeZone:(out NSTimeZone **)outTimeZone;
+/*!
+ *	@brief	Parse a string into a single date, identified by an NSDate object.
+ *
+ *	@param	string	The string to parse. Must represent a date in one of the ISO 8601 formats.
+ *	@param	outTimeZone	If non-`NULL`, an NSTimeZone object or `nil` will be stored here, depending on whether the string specified a time zone.
+ *	@param	outRange	If non-`NULL`, an NSRange structure will be stored here, identifying the substring of `string` that specified the date.
+ *	@returns	An NSDate object containing most of the information parsed from the string, aside from the time zone.
+ */
 - (NSDate *) dateFromString:(NSString *)string timeZone:(out NSTimeZone **)outTimeZone range:(out NSRange *)outRange;
 
 #pragma mark Unparsing
 
+/*!
+ *	@brief	Which ISO 8601 format to format dates in.
+ *
+ *	@details	See ISO8601DateFormat for possible values.
+ */
 @property ISO8601DateFormat format;
+/*!
+ *	@brief	Whether strings should include time of day.
+ *
+ *	@details	If `NO`, strings include only the date, nothing after it.
+ */
 @property BOOL includeTime;
+/*!
+ *	@brief	The character to use to separate components of the time of day.
+ *
+ *	@details	Defaults to ISO8601DefaultTimeSeparatorCharacter. Must not be zero, or unparsing methods will raise an exception.
+ */
 @property unichar timeSeparator;
+/*!
+ *	@brief	The character to use to separate the hour and minute in a time zone specification.
+ *
+ *	@details	If zero, no separator is inserted into time zone specifications. Defaults to zero.
+ */
 @property unichar timeZoneSeparator;
 
+/*!
+ *	@brief	Produce a string that represents a date in UTC.
+ *
+ *	@param	date	The string to parse. Must represent a date in one of the ISO 8601 formats.
+ *	@returns	A string that represents the date in UTC.
+ */
 - (NSString *) stringFromDate:(NSDate *)date;
+/*!
+ *	@brief	Produce a string that represents a date.
+ *
+ *	@param	date	The string to parse. Must represent a date in one of the ISO 8601 formats.
+ *	@param	timeZone	An NSTimeZone object identifying the time zone in which to specify the date.
+ *	@returns	A string that represents the date in the requested time zone, if possible.
+ *
+ *	@details	Not all dates are representable in all time zones (because of historical calendar changes, such as transitions from the Julian to the Gregorian calendar).
+ *	For an example, see http://stackoverflow.com/questions/18663407/date-formatter-returns-nil-for-june .
+ *	The ISO 8601 formatter *should* return `nil` in such cases.
+ */
 - (NSString *) stringFromDate:(NSDate *)date timeZone:(NSTimeZone *)timeZone;
 
 @end
