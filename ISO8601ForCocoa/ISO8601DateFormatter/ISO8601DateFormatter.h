66a62aa211e5f50f625b45bf76bb3f81add5d2e2
vasarhelyia <vasarhelyi.agi@gmail.com>
2014-12-30 14:36:16 +0100

    Added framework build target for Carthage support.

---
+++ ISO8601ForCocoa/ISO8601DateFormatter/ISO8601DateFormatter.h
@@ -0,0 +1,19 @@
+//
+//  ISO8601DateFormatter.h
+//  ISO8601DateFormatter
+//
+//  Created by Agnes Vasarhelyi on 30/12/14.
+//  Copyright (c) 2014 Peter Hosey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISO8601DateFormatter.
+FOUNDATION_EXPORT double ISO8601DateFormatterVersionNumber;
+
+//! Project version string for ISO8601DateFormatter.
+FOUNDATION_EXPORT const unsigned char ISO8601DateFormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISO8601DateFormatter/PublicHeader.h>
+
+
